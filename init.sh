cb7212f9d13bc8d723a133f6ec3b9c3f8b8dba18
Vallabh Kansagara <vrkansagara@gmail.com>
2021-11-01 07:45:33 +0530

    initial commit for the configuration

---
+++ init.sh
@@ -0,0 +1,42 @@
+
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
+
+echo " "
+export DEBIAN_FRONTEND=noninteractive
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+SCRIPT=$(readlink -f "")
+SCRIPTDIR=$(dirname "$SCRIPT")
+
+if [ "$(whoami)" != "root" ]; then
+	SUDO=sudo
+fi
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :- Suckless terminal for DWM and general purpose.
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# ${SUDO} sudo apt-get install --yes -q  --no-install-recommends 
+
+FILES="patches/*.diff"
+for f in $FILES; do
+		echo "Applying path for the [ $f ]"
+		dos2unix $f
+		patch --merge=diff3 -i $f
+		sleep 1
+done
+
+# Give current user permission to work with source
+${SUDO} chown $USER  -Rf .
+${SUDO} chgrp $USER  -Rf .
+
+${SUDO} rm -rf /usr/share/bin/st
+make clean
+make
+${SUDO} make install
+${SUDO} make clean
+
+echo "Suckless termina is ready to use ...... [DONE]"
+
+exit 0
