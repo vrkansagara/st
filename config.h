9c00e745bd09556cd8c1d96bf42076d5c99a553e
Vallabh Kansagara <vrkansagara@gmail.com>
2021-11-01 18:37:27 +0530

    patch : font2 applied

--- config.h
+++ config.h
@@ -5,9 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Code Medium:size=14:antialias=true:autohint=true";
 static char *font2[] = {
+	"Fira Code Medium:size=14:antialias=true:autohint=true",
 	"Liberation Mono:pixelsize=12:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 
 /* disable bold, italic and roman fonts globally */
